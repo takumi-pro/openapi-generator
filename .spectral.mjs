d44c4cf634890df0d9556c9fbcdfe31faef3dc26
takumidiary.0927@gmail.com <takumidiary.0927@gmail.com>
2023-08-20 20:39:31 +0900

    todo api

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNDk1Mg/spectral.js?branch_slug=main&token=0ee46bcd-d1c0-4762-8fce-1b2dddf15720";
+export default { extends: ruleset };
